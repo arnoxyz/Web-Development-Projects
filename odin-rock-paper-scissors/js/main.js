da000fa535f7a078866593c0d30fd483a9b30fbd
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-07 01:09:16 +0200

    odin-rock-paper-scissors add getComputerChoice()

--- odin-rock-paper-scissors/js/main.js
+++ odin-rock-paper-scissors/js/main.js
@@ -1,1 +1,24 @@
 console.log("Hello World!")
+let computerChoice
+
+// Function that gives a random number 0,1, or 2 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+// returns computerChoide
+function getComputerChoice() {	
+    let choice = getRandomInt(3);
+    if (choice === 0) {
+        return 'r';
+    }
+    if (choice === 1) {
+        return 'p';
+    }
+    if (choice === 2) {
+        return 's';
+    }
+}
+
+console.log(getComputerChoice());
