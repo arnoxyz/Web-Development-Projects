ff8b388ae3e5f071014258a4ac118b94b4672d7d
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-07 11:28:38 +0200

    odin-rock-paper-scissors add getPlayerChoice()

--- odin-rock-paper-scissors/js/main.js
+++ odin-rock-paper-scissors/js/main.js
@@ -1,13 +1,10 @@
-console.log("Hello World!")
-let computerChoice
+console.log("Welcome to Rock, Paper, Scissors:")
 
 // Function that gives a random number 0,1, or 2 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-
-// returns computerChoide
 function getComputerChoice() {	
     let choice = getRandomInt(3);
     if (choice === 0) {
@@ -21,4 +18,39 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerChoice = "";
+
+    while (true) {
+        let inputText = prompt("Please insert your choice: (R)ock, (P)aper, (S)cissor").toLowerCase();
+        if (inputText.includes("rock") || inputText==="r") {
+            playerChoice = "r";
+            break;
+           }
+        if (inputText.includes("paper") || inputText==="p") {
+            playerChoice = "p";
+            break;
+           }
+        if (inputText.includes("scissor") || inputText==="s") {
+            playerChoice = "s";
+            break;
+           }
+        console.log("wrong input, try that again...");
+        }
+
+    return playerChoice;
+    }
+
+function gameLogic(computerChoice, playerChoice) {
+    console.log(computerChoice);
+    console.log(playerChoice);
+    }
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+let playerChoice = getPlayerChoice();
+console.log(playerChoice);
+
+gameLogic(computerChoice, playerChoice);
+
