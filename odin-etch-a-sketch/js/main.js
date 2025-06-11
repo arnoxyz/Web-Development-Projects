c439bdbca8451927ec9ac4bf21a22dc5a6d989e7
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-11 14:32:32 +0200

    odin-etch-a-sketch one draw board

--- odin-etch-a-sketch/js/main.js
+++ odin-etch-a-sketch/js/main.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+
+
+function createBoard(lines) {
+    for (let i = 0; i < lines; i++){
+        const element = document.getElementById("game-board");
+        const newDiv = document.createElement("div");
+        newDiv.textContent = "";
+        newDiv.style.backgroundColor = "black";
+        newDiv.style.padding = "0px";
+        newDiv.style.margin = "0px";
+        newDiv.className = "game-board-line";
+        element.append(newDiv);
+   }
+
+   createBoardElements(lines);
+}
+
+function createBoardElements(elementsPerLine) {
+    const elements = Array.from(document.getElementsByClassName("game-board-line"));
+
+    elements.forEach(element => {    
+        for (let i = 0; i < elementsPerLine; i++){
+            const newDiv = document.createElement("div");
+            newDiv.textContent = "";
+            newDiv.style.backgroundColor = "red";
+            newDiv.style.padding = "10px";
+            newDiv.style.margin = "0px";
+            newDiv.className = "game-board-element";
+            element.append(newDiv);
+        }
+    });
+}
+
+
+function main(){
+    // Init-Board
+    createBoard(16);
+}
+
+main();
+
