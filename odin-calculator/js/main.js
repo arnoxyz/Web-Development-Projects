963046211273e88393da42dde9504c3ef54d3087
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-18 23:10:04 +0200

    odin-calculator add operate function

--- odin-calculator/js/main.js
+++ odin-calculator/js/main.js
@@ -1,23 +1,43 @@
 console.log("Hello World");
 
-function add(a, b) { return a+b; }
-function sub(a, b) { return a-b; }
-function multiply(a, b) { return a*b; }
-function divide(a, b) { return a/b; }
-function mod(a, b) { return a%b; }
+function operate(a,b,op) {
+    switch (op) {
+        case '+':
+          return a + b;
+        case '-':
+          return a - b;
+        case '*':
+          return a * b;
+        case '/':
+          if (b === 0) { return 'Error: Division by zero'; }
+          return a / b;
+        case '%':
+          if (b === 0) { return 'Error: Division by zero'; }
+          return a %  b;
+    default:
+      return `Error: Unknown operator "${operator}"`;
+  }
+}
 
 function main(){
-    //Testing basic math functions:
-    console.log(add(1,2));
-    console.log(add(4,-2));
-    console.log(sub(4,2));
-    console.log(sub(1,2));
-    console.log(multiply(4,2));
-    console.log(multiply(2,8));
-    console.log(divide(10,2));
-    console.log(divide(10,3));
-    console.log(mod(10,2));
-    console.log(mod(11,2));
+    firstNumber = 1
+    secondNumber = 2
+    operation = "+"
+    solution = 0
+
+    //Basic Calculator Steps:
+        //1) Press: Insert First Number
+        //firstNumber =
+        //2) Press: Operation {+,-,*,/,mod}
+        //operation =
+        //3) Press: Insert Second Number
+        //secondnumber =
+        //4)Press: =
+        solution = operate(firstNumber, secondNumber, operation);
+        console.log(solution)
+
+    //Press: C = deletes and restarts from Insert First Number
+    //reset();
 }
 
 
