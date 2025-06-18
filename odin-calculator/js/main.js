85da438a8bd2922a286b246b8cc408bbaa015bc8
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-18 22:59:20 +0200

    odin-calculator done basic operations

--- odin-calculator/js/main.js
+++ odin-calculator/js/main.js
@@ -1,1 +1,25 @@
 console.log("Hello World");
+
+function add(a, b) { return a+b; }
+function sub(a, b) { return a-b; }
+function multiply(a, b) { return a*b; }
+function divide(a, b) { return a/b; }
+function mod(a, b) { return a%b; }
+
+function main(){
+    //Testing basic math functions:
+    console.log(add(1,2));
+    console.log(add(4,-2));
+    console.log(sub(4,2));
+    console.log(sub(1,2));
+    console.log(multiply(4,2));
+    console.log(multiply(2,8));
+    console.log(divide(10,2));
+    console.log(divide(10,3));
+    console.log(mod(10,2));
+    console.log(mod(11,2));
+}
+
+
+
+main();
