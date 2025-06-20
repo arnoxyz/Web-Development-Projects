117147e8e8bd1872fb09c3f19cb2a0fbe10a7035
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-20 16:29:51 +0200

    odin-calculator done genNumberBoard

--- odin-calculator/js/main.js
+++ odin-calculator/js/main.js
@@ -1,8 +1,49 @@
 console.log("Hello World");
 
-function gen_calculator() {
+function genElements() {
+    genNumberBoard();
+}
+
+function genNumberBoard() {
     const main = document.querySelector("#main");
-    console.log(main);
+    const numberBoard = document.createElement('div');
+    numberBoard.id = "number-board";
+    numberBoard.textContent = "";
+    main.appendChild(numberBoard);
+
+    const row1 = genRow("row1");
+    const row2 = genRow("row2");
+    const row3 = genRow("row3");
+    const row4 = genRow("row4");
+    numberBoard.appendChild(row1);
+    numberBoard.appendChild(row2);
+    numberBoard.appendChild(row3);
+    numberBoard.appendChild(row4);
+
+    row1.appendChild(genDiv(1));
+    row1.appendChild(genDiv(2));
+    row1.appendChild(genDiv(3));
+    row2.appendChild(genDiv(4));
+    row2.appendChild(genDiv(5));
+    row2.appendChild(genDiv(6));
+    row3.appendChild(genDiv(7));
+    row3.appendChild(genDiv(8));
+    row3.appendChild(genDiv(9));
+    row4.appendChild(genDiv(0));
+}
+
+function genDiv(number){
+  const newDiv = document.createElement('div');
+  newDiv.id = number;
+  newDiv.textContent = number;
+  return newDiv;
+}
+
+function genRow(number){
+  const newDiv = document.createElement('div');
+  newDiv.id = number;
+  newDiv.textcontent = "";
+  return newDiv;
 }
 
 function operate(a,b,op) {
@@ -26,13 +67,13 @@ function operate(a,b,op) {
 
 function main(){
     //Gen UX
-    gen_calculator();
+    genElements();
 
 
-    firstNumber = 1
-    secondNumber = 2
-    operation = "+"
-    solution = 0
+    firstNumber = 1;
+    secondNumber = 2;
+    operation = "+";
+    solution = 0;
 
     //Basic Calculator Steps:
         //1) Press: Insert First Number
