b3ed2628b9326943457751cc04695870e82ecc10
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-12 11:06:34 +0200

    odin-library add book class and some books I like

--- odin-library/js/main.js
+++ odin-library/js/main.js
@@ -1,5 +1,30 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  toString() {
+    return `${this.title} by ${this.author}, published in ${this.year}`;
+  }
+}
+
+//Add some books that I like
+const books = [
+  new Book("Atomic Habits", "James Clear", 2018),
+  new Book("Ham on Rye", "Charles Bukowski", 1982),
+  new Book("Im Westen nichts Neues", "Erich Maria Remarque", "1928"),
+  new Book("Gut gegen Nordwind", "Daniel Glattauer", "2006"),
+  new Book("Sechs Österreicher unter den ersten fünf", "Dirk Stermann", "2010")
+];
+
 function main(){
-    console.log("Hello World!");
+
+    console.log("Print all saved books");
+    books.forEach(book => {
+        console.log(book.toString());
+    });
 }
 
 main();
