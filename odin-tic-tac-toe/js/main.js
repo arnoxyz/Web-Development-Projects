b2bdc5a13ddd51b0515ff699bde1ae1bcf2c5193
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-24 00:06:35 +0200

    odin-tic-tac-toe add createUIBoard()

--- odin-tic-tac-toe/js/main.js
+++ odin-tic-tac-toe/js/main.js
@@ -56,9 +56,48 @@ const gameBoard = (function () {
     };
 })();
 
+function createUIBoard(){
+    const mainDiv = document.getElementById("main");
+    //Board Cells:
+    // 0, 1, 2
+    // 3, 4, 5
+    // 6, 7, 8
+
+    const row0 = document.createElement('div');
+    row0.id = row0;
+    row0.appendChild(createDiv(0));
+    row0.appendChild(createDiv(1));
+    row0.appendChild(createDiv(2));
+
+    const row1 = document.createElement('div');
+    row1.id = row0;
+    row1.appendChild(createDiv(3));
+    row1.appendChild(createDiv(4));
+    row1.appendChild(createDiv(5));
+
+    const row2 = document.createElement('div');
+    row2.id = row0;
+    row2.appendChild(createDiv(6));
+    row2.appendChild(createDiv(7));
+    row2.appendChild(createDiv(8));
+
+    mainDiv.appendChild(row0);
+    mainDiv.appendChild(row1);
+    mainDiv.appendChild(row2);
+}
+
+function createDiv(number){
+    const div = document.createElement('div');
+    div.textContent = number;
+    div.className = 'board';
+    div.id = number;
+    return div;
+}
 
 
 function main(){
+    createUIBoard();
+
     //test1
     gameBoard.setCell(0, "X");
     gameBoard.setCell(4, "X");
